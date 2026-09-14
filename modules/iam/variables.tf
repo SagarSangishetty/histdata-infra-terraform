67680e3d27e0f8aab025a66f41972ed545a6112c
SagarSangishetty <shettysagar272@gmail.com>
2026-09-15 00:22:16 +0530

    Added required variable for the kubectl access

--- modules/iam/variables.tf
+++ modules/iam/variables.tf
@@ -18,3 +18,7 @@ variable "github_deploy_subject" {
   type        = string
   description = "OIDC subject for the Kubernetes deployment repository"
 }
+variable "sso_admin_role_arn" {
+  description = "IAM Identity Center role allowed to administer the EKS cluster"
+  type        = string
+}
